8661f8e8dc4959d648ace96217549449d8125c81
lucasmoraist <luksmnt1101@gmail.com>
2026-01-14 21:15:42 -0300

    first commit

---
+++ src/test/java/com/lucasmoraist/wallet_core/WalletCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmoraist.wallet_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
