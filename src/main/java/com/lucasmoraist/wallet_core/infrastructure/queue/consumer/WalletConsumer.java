281891fdc5f11880d5a6b6a29d62720440d1ff37
lucasmoraist <luksmnt1101@gmail.com>
2026-01-15 16:38:23 -0300

    feat: add WalletConsumer class for processing payment messages

---
+++ src/main/java/com/lucasmoraist/wallet_core/infrastructure/queue/consumer/WalletConsumer.java
@@ -0,0 +1,23 @@
+package com.lucasmoraist.wallet_core.infrastructure.queue.consumer;
+
+import com.lucasmoraist.wallet_core.application.messages.OrchestrationTransfer;
+import com.lucasmoraist.wallet_core.domain.message.PaymentMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+
+import java.util.function.Consumer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WalletConsumer {
+
+    private final OrchestrationTransfer orchestrationTransfer;
+
+    @Bean
+    public Consumer<Message<PaymentMessage>> fromProcessTransfer() {
+        return orchestrationTransfer::execute;
+    }
+
+}
