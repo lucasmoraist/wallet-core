3caa3ec9f40f5a4c90b403d6506c9454d90fdfe8
lucasmoraist <luksmnt1101@gmail.com>
2026-01-14 22:53:35 -0300

    feat: add method to determine role based on document length in RolesEnum

--- src/main/java/com/lucasmoraist/wallet_core/domain/enums/RolesEnum.java
+++ src/main/java/com/lucasmoraist/wallet_core/domain/enums/RolesEnum.java
@@ -2,5 +2,16 @@ package com.lucasmoraist.wallet_core.domain.enums;
 
 public enum RolesEnum {
     COMMON,
-    MERCHANT
+    MERCHANT;
+
+    public static RolesEnum getRoleByDocument(String cpfCnpj) {
+        String cleanedDocument = cpfCnpj.replaceAll("\\D", "");
+        if (cleanedDocument.length() == 11) {
+            return COMMON;
+        } else if (cleanedDocument.length() == 14) {
+            return MERCHANT;
+        } else {
+            throw new IllegalArgumentException("Invalid document length");
+        }
+    }
 }
