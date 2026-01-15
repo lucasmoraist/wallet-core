407c12a05eccc15156c58b3745fc10f087ae6a35
lucasmoraist <luksmnt1101@gmail.com>
2026-01-15 10:31:06 -0300

    feat: add createWalletForUser method to WalletPersistence for user wallet creation

--- src/main/java/com/lucasmoraist/wallet_core/application/gateway/WalletPersistence.java
+++ src/main/java/com/lucasmoraist/wallet_core/application/gateway/WalletPersistence.java
@@ -1,5 +1,6 @@
 package com.lucasmoraist.wallet_core.application.gateway;
 
+import com.lucasmoraist.wallet_core.domain.model.User;
 import com.lucasmoraist.wallet_core.domain.model.Wallet;
 
 import java.util.UUID;
@@ -7,5 +8,6 @@ import java.util.UUID;
 public interface WalletPersistence {
 
     Wallet findById(UUID walletId);
+    Wallet createWalletForUser(User user);
 
 }
--- src/main/java/com/lucasmoraist/wallet_core/infrastructure/database/persistence/WalletPersistenceImpl.java
+++ src/main/java/com/lucasmoraist/wallet_core/infrastructure/database/persistence/WalletPersistenceImpl.java
@@ -1,14 +1,20 @@
 package com.lucasmoraist.wallet_core.infrastructure.database.persistence;
 
 import com.lucasmoraist.wallet_core.application.gateway.WalletPersistence;
+import com.lucasmoraist.wallet_core.domain.model.User;
 import com.lucasmoraist.wallet_core.domain.model.Wallet;
+import com.lucasmoraist.wallet_core.infrastructure.database.entity.UserEntity;
 import com.lucasmoraist.wallet_core.infrastructure.database.entity.WalletEntity;
 import com.lucasmoraist.wallet_core.infrastructure.database.repository.WalletRepository;
+import com.lucasmoraist.wallet_core.infrastructure.mapper.UserMapper;
 import com.lucasmoraist.wallet_core.infrastructure.mapper.WalletMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.UUID;
 
 @Log4j2
@@ -21,6 +27,20 @@ public class WalletPersistenceImpl implements WalletPersistence {
     @Override
     public Wallet findById(UUID walletId) {
         return WalletMapper.toDomain(findEntityById(walletId));
+    }
+
+    @Override
+    @Transactional
+    public Wallet createWalletForUser(User user) {
+        UserEntity userEntity = UserMapper.toEntity(user);
+        WalletEntity walletEntity = WalletEntity.builder()
+                .user(userEntity)
+                .balance(BigDecimal.ZERO)
+                .version(1)
+                .transactions(new ArrayList<>())
+                .build();
+        WalletEntity walletSaved = this.walletRepository.save(walletEntity);
+        return WalletMapper.toDomain(walletSaved);
     }
 
     private WalletEntity findEntityById(UUID walletId) {
