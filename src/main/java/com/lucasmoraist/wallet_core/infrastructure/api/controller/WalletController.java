9284e363136969029c396b4ca9614d2f470051a8
lucasmoraist <luksmnt1101@gmail.com>
2026-01-15 10:31:40 -0300

    feat: add addWallet method to WalletController for wallet creation

--- src/main/java/com/lucasmoraist/wallet_core/infrastructure/api/controller/WalletController.java
+++ src/main/java/com/lucasmoraist/wallet_core/infrastructure/api/controller/WalletController.java
@@ -1,14 +1,20 @@
 package com.lucasmoraist.wallet_core.infrastructure.api.controller;
 
+import com.lucasmoraist.wallet_core.application.usecases.wallet.AddWalletCase;
 import com.lucasmoraist.wallet_core.application.usecases.wallet.ConsultBalanceCase;
+import com.lucasmoraist.wallet_core.domain.model.Wallet;
 import com.lucasmoraist.wallet_core.infrastructure.api.documentation.routes.WalletDocumentationRoutes;
 import com.lucasmoraist.wallet_core.infrastructure.api.dto.wallet.BalanceDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.net.URI;
 import java.util.UUID;
 
 @RestController
@@ -17,9 +23,19 @@ import java.util.UUID;
 public class WalletController implements WalletDocumentationRoutes {
 
     private final ConsultBalanceCase consultBalanceCase;
+    private final AddWalletCase addWalletCase;
 
     @Override
-    public ResponseEntity<BalanceDTO> consultBalance(UUID walletId) {
+    @PostMapping("/users/{userId}")
+    public ResponseEntity<Void> addWallet(@PathVariable UUID userId) {
+        Wallet wallet = this.addWalletCase.execute(userId);
+        URI location = URI.create(String.format("/api/v1/wallets/%s", wallet.id()));
+        return ResponseEntity.created(location).build();
+    }
+
+    @Override
+    @GetMapping("/{walletId}/consult-balance")
+    public ResponseEntity<BalanceDTO> consultBalance(@PathVariable UUID walletId) {
         BigDecimal balance = this.consultBalanceCase.execute(walletId).balance();
         return ResponseEntity.ok().body(new BalanceDTO(balance));
     }
--- src/main/java/com/lucasmoraist/wallet_core/infrastructure/api/documentation/routes/WalletDocumentationRoutes.java
+++ src/main/java/com/lucasmoraist/wallet_core/infrastructure/api/documentation/routes/WalletDocumentationRoutes.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 
 public interface WalletDocumentationRoutes {
 
+    ResponseEntity<Void> addWallet(UUID userId);
     ResponseEntity<BalanceDTO> consultBalance(UUID walletId);
 
 }
