6ac81ed79808d9e0c6d5e15c4c2df0dcd692459d
lucasmoraist <luksmnt1101@gmail.com>
2026-01-18 12:20:26 -0300

    feat: add H2 in-memory database configuration for testing and enable test profile

--- src/test/java/com/lucasmoraist/wallet_core/WalletCoreApplicationTests.java
+++ src/test/java/com/lucasmoraist/wallet_core/WalletCoreApplicationTests.java
@@ -2,12 +2,15 @@ package com.lucasmoraist.wallet_core;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WalletCoreApplicationTests {
 
 	@Test
 	void contextLoads() {
+        WalletCoreApplication.main(new String[0]);
 	}
 
 }
