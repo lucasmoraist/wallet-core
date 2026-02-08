32cf8a6dcc0a30145cf96e86cb992b90ce0a54e6
lucasmoraist <luksmnt1101@gmail.com>
2026-02-08 00:37:53 -0300

    feat: add Redis test container configuration for integration tests

--- src/test/java/com/lucasmoraist/wallet_core/WalletCoreApplicationTests.java
+++ src/test/java/com/lucasmoraist/wallet_core/WalletCoreApplicationTests.java
@@ -1,11 +1,14 @@
 package com.lucasmoraist.wallet_core;
 
+import com.lucasmoraist.wallet_core.config.RedisTestContainerConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(RedisTestContainerConfig.class)
 class WalletCoreApplicationTests {
 
 	@Test
---
+++ src/test/java/com/lucasmoraist/wallet_core/config/RedisTestContainerConfig.java
@@ -0,0 +1,19 @@
+package com.lucasmoraist.wallet_core.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class RedisTestContainerConfig {
+
+    @Bean
+    @ServiceConnection(name = "redis")
+    public GenericContainer<?> redisContainer() {
+        return new GenericContainer<>(DockerImageName.parse("redis:8.4-alpine"))
+                .withExposedPorts(6379);
+    }
+
+}
