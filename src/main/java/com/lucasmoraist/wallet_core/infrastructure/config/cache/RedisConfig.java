05105c2615c0219c1b020620b900308e11543bfb
lucasmoraist <luksmnt1101@gmail.com>
2026-02-08 00:37:25 -0300

    feat: add profile annotation to Redis configuration for non-test environments

--- src/main/java/com/lucasmoraist/wallet_core/infrastructure/config/cache/RedisConfig.java
+++ src/main/java/com/lucasmoraist/wallet_core/infrastructure/config/cache/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -16,6 +17,7 @@ import java.time.Duration;
 
 @EnableCaching
 @Configuration
+@Profile("!test")
 public class RedisConfig {
 
     @Value("${spring.data.cache.redis.time-to-live-seconds}")
