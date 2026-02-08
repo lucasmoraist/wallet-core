0c318e5485dc0b6084bc24f998fae1a1db19806e
lucasmoraist <luksmnt1101@gmail.com>
2026-02-08 01:12:31 -0300

    feat: add ADMIN role to RolesEnum for enhanced role management

--- src/main/java/com/lucasmoraist/wallet_core/domain/enums/RolesEnum.java
+++ src/main/java/com/lucasmoraist/wallet_core/domain/enums/RolesEnum.java
@@ -2,6 +2,7 @@ package com.lucasmoraist.wallet_core.domain.enums;
 
 public enum RolesEnum {
     COMMON,
+    ADMIN,
     MERCHANT;
 
     public static RolesEnum getRoleByDocument(String cpfCnpj) {
