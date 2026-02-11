2bc5165d4dbf29b56ab5afc3b1a9966d31d3ad09
lucasmoraist <luksmnt1101@gmail.com>
2026-02-10 23:39:18 -0300

    feat: enhance CreateUserCase to include SecurityGateway for password encryption

--- src/main/java/com/lucasmoraist/wallet_core/application/usecases/user/CreateUserCase.java
+++ src/main/java/com/lucasmoraist/wallet_core/application/usecases/user/CreateUserCase.java
@@ -1,5 +1,6 @@
 package com.lucasmoraist.wallet_core.application.usecases.user;
 
+import com.lucasmoraist.wallet_core.application.gateway.SecurityGateway;
 import com.lucasmoraist.wallet_core.application.gateway.UserPersistence;
 import com.lucasmoraist.wallet_core.domain.enums.RolesEnum;
 import com.lucasmoraist.wallet_core.domain.model.User;
@@ -7,9 +8,11 @@ import com.lucasmoraist.wallet_core.domain.model.User;
 public class CreateUserCase {
 
     private final UserPersistence userPersistence;
+    private final SecurityGateway securityGateway;
 
-    public CreateUserCase(UserPersistence userPersistence) {
+    public CreateUserCase(UserPersistence userPersistence, SecurityGateway securityGateway) {
         this.userPersistence = userPersistence;
+        this.securityGateway = securityGateway;
     }
 
     public User execute(String fullName, String cpfCnpj, String email, String password) {
@@ -18,7 +21,7 @@ public class CreateUserCase {
                 fullName,
                 cpfCnpj,
                 email,
-                password,
+                this.securityGateway.encryptPassword(password),
                 RolesEnum.getRoleByDocument(cpfCnpj),
                 null,
                 null,
