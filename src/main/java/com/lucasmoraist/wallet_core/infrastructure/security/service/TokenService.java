c9403730501a6e7dc143f0a9b703b22b6471fe39
lucasmoraist <luksmnt1101@gmail.com>
2026-02-16 17:25:02 -0300

    feat: remove unused DecodedJWT import from TokenService

--- src/main/java/com/lucasmoraist/wallet_core/infrastructure/security/service/TokenService.java
+++ src/main/java/com/lucasmoraist/wallet_core/infrastructure/security/service/TokenService.java
@@ -4,7 +4,6 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import com.lucasmoraist.wallet_core.application.gateway.TokenGateway;
 import com.lucasmoraist.wallet_core.domain.exception.JwtException;
 import com.lucasmoraist.wallet_core.domain.model.User;
