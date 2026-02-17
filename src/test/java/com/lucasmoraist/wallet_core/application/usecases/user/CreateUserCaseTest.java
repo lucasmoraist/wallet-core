2ead1407302e2643803d56fd05556a2b3e73de88
lucasmoraist <luksmnt1101@gmail.com>
2026-02-16 21:00:01 -0300

    feat: update CreateUserCaseTest and UserFactory to support user creation with UUID and wallet integration

--- src/test/java/com/lucasmoraist/wallet_core/application/usecases/user/CreateUserCaseTest.java
+++ src/test/java/com/lucasmoraist/wallet_core/application/usecases/user/CreateUserCaseTest.java
@@ -1,6 +1,8 @@
 package com.lucasmoraist.wallet_core.application.usecases.user;
 
+import com.lucasmoraist.wallet_core.application.gateway.SecurityGateway;
 import com.lucasmoraist.wallet_core.application.gateway.UserPersistence;
+import com.lucasmoraist.wallet_core.application.usecases.wallet.AddWalletCase;
 import com.lucasmoraist.wallet_core.domain.enums.RolesEnum;
 import com.lucasmoraist.wallet_core.domain.model.User;
 import com.lucasmoraist.wallet_core.factory.UserFactory;
@@ -11,9 +13,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -23,6 +28,10 @@ class CreateUserCaseTest {
 
     @Mock
     UserPersistence userPersistence;
+    @Mock
+    SecurityGateway securityGateway;
+    @Mock
+    AddWalletCase addWalletCase;
     @InjectMocks
     CreateUserCase createUserCase;
 
@@ -30,10 +39,12 @@ class CreateUserCaseTest {
     @DisplayName("Should create a new user COMMON successfully")
     void case01() {
         String cpfCnpj = "11122233344";
+        UUID userId = UUID.randomUUID();
 
-        User userExpected = UserFactory.createUser(cpfCnpj);
+        User userExpected = UserFactory.createUser(userId, cpfCnpj);
 
         when(userPersistence.save(any(User.class))).thenReturn(userExpected);
+        when(securityGateway.encryptPassword(anyString())).thenReturn(userExpected.password());
 
         User userResponse = this.createUserCase.execute(userExpected.fullName(), cpfCnpj, userExpected.email(), userExpected.password());
 
@@ -43,17 +54,20 @@ class CreateUserCaseTest {
         assertEquals(userExpected.email(), userResponse.email());
         assertEquals(userExpected.password(), userResponse.password());
         assertEquals(RolesEnum.COMMON, userResponse.role());
-        verify(userPersistence, times(1)).save(userExpected);
+        verify(userPersistence, times(1)).save(any());
+        verify(addWalletCase, times(1)).execute(userId);
     }
 
     @Test
     @DisplayName("Should create a new user MERCHANT successfully")
     void case02() {
         String cpfCnpj = "11222333000181";
+        UUID userId = UUID.randomUUID();
 
-        User userExpected = UserFactory.createUser(cpfCnpj);
+        User userExpected = UserFactory.createUser(userId, cpfCnpj);
 
         when(userPersistence.save(any(User.class))).thenReturn(userExpected);
+        when(securityGateway.encryptPassword(anyString())).thenReturn(userExpected.password());
 
         User userResponse = this.createUserCase.execute(userExpected.fullName(), cpfCnpj, userExpected.email(), userExpected.password());
 
@@ -63,7 +77,8 @@ class CreateUserCaseTest {
         assertEquals(userExpected.email(), userResponse.email());
         assertEquals(userExpected.password(), userResponse.password());
         assertEquals(RolesEnum.MERCHANT, userResponse.role());
-        verify(userPersistence, times(1)).save(userExpected);
+        verify(userPersistence, times(1)).save(any());
+        verify(addWalletCase, times(1)).execute(userId);
     }
 
 }
--- src/test/java/com/lucasmoraist/wallet_core/factory/UserFactory.java
+++ src/test/java/com/lucasmoraist/wallet_core/factory/UserFactory.java
@@ -21,20 +21,6 @@ public final class UserFactory {
         );
     }
 
-    public static User createUser(String cpfCnpj) {
-        return new User(
-                null,
-                "John Doe",
-                cpfCnpj,
-                "johndoe@example.com",
-                "password123",
-                RolesEnum.getRoleByDocument(cpfCnpj),
-                null,
-                null,
-                null
-        );
-    }
-
     public static User createUser(UUID userId) {
         return new User(
                 userId,
@@ -43,6 +29,20 @@ public final class UserFactory {
                 "johndoe@example.com",
                 "password123",
                 RolesEnum.getRoleByDocument("11122233344"),
+                null,
+                null,
+                null
+        );
+    }
+
+    public static User createUser(UUID userId, String cpfCnpj) {
+        return new User(
+                userId,
+                "John Doe",
+                cpfCnpj,
+                "johndoe@example.com",
+                "password123",
+                RolesEnum.getRoleByDocument(cpfCnpj),
                 null,
                 null,
                 null
